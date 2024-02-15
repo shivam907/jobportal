710794c0441f6ee629e6ec721c9763f514530c51
shivam907 <shivamkaushal907@gmail.com>
2024-02-16 02:47:31 +0530

    hero section done

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,25 @@
 import classes from "./page.module.css";
-
+import Button1 from "@/components/Buttons/Button1";
 export default function Home() {
   return (
     <main className={classes.main}>
-      <div className={classes.hero}></div>
+      <div className={classes.hero}>
+        <div className={classes.bar}>
+          <div className={classes.input}>
+          <img src="./search.svg" alt="" />
+            <input type="text" name="" id="" placeholder="Search" />
+          </div>
+          <div className={classes.input}>
+          <img src="./search.svg" alt="" />
+            <input type="text" name="" id="" placeholder="Search" />
+          </div>
+          <div className={classes.input}>
+          <img src="./search.svg" alt="" />
+            <input type="text" name="" id="" placeholder="Search" />
+          </div>
+          <Button1>Search</Button1>
+        </div>
+      </div>
     </main>
   );
 }
