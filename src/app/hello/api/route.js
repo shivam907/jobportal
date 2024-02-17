b58d7305df359ba33a1d37a50946e80b607ea8f2
shivam907 <shivamkaushal907@gmail.com>
2024-02-17 21:40:49 +0530

    added backend API's

--- src/app/hello/api/route.js
+++ src/app/hello/api/route.js
@@ -1,8 +1,3 @@
-export  async function GET() {
-  // if (req.method === "GET") {
-  return Response.json({ data: [1,2,3] })
-  // }
-  // else {
-  //   res.status(405).json({ success: false, message: 'Method Not Allowed' });
-  // }
+export async function GET() {
+  return Response.json({ data: [1, 2, 3] });
 }
