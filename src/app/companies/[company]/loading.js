d8be3cc083f11bfbe1e95f29c58153890305a79e
shivam907 <shivamkaushal907@gmail.com>
2024-02-23 02:58:38 +0530

    all levers API working

---
+++ src/app/companies/[company]/loading.js
@@ -0,0 +1,11 @@
+"use client"
+import React from 'react'
+
+const loading = () => {
+    console.log("loading")
+  return (
+    <div>loading</div>
+  )
+}
+
+export default loading
--- src/app/companies/[company]/page.js
+++ src/app/companies/[company]/page.js
@@ -1,8 +1,54 @@
+"use client"
 import React from 'react'
-
+import data from "@/data/company.json";
+import JobBox from '@/components/Job/JobBox';
 const page = (props) => {
+    const [job, setJob] = React.useState();
+    React.useEffect(()=>{
+        const fun = async()=>{
+        console.log(data.data[props.params.company])
+        const a = await fetch(data.data[props.params.company]);
+        const b = await a.json();
+        let arr = [];
+        b.forEach((i) => {
+            console.log(i)
+            if(i.postings){
+                console.log("posting")
+                i.postings.forEach((j) => {
+                arr.push(
+                    <JobBox
+                    jobName={j.text}
+                    date={j.createdAt}
+                    companyName="Clevertap"
+                    location={j.categories.location}
+                    role={j.categories.commitment}
+                    description={j.descriptionPlain.slice(0, 100)}
+                    link={j.applyUrl}
+                    />
+                );
+            });
+        }
+        else {
+            console.log("no posting")
+            arr.push(
+              <JobBox
+                jobName={i.text}
+                date={i.createdAt}
+                companyName="Coupa"
+                location={i.categories.location}
+                role={i.categories.commitment}
+                description={i.descriptionPlain.slice(0, 100)}
+                link={i.applyUrl}
+              />
+            );
+        }
+        });
+        setJob(arr)
+        }
+        fun()
+    },[])
   return (
-    <div>{props.params.company}</div>
+    <>{job}</>
   )
 }
 
