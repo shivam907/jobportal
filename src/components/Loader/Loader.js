f3f0d3edba09a7a6749d6ffaa10644d37c355278
shivam907 <shivamkaushal907@gmail.com>
2024-02-23 22:21:33 +0530

    Added the Loader

---
+++ src/components/Loader/Loader.js
@@ -0,0 +1,11 @@
+import React from "react";
+import classes from "./Loader.module.css"
+const Loader = () => {
+  return (
+    <div className={classes["spinner-container"]}>
+      <div className={classes["loading-spinner"]}></div>
+    </div>
+  );
+};
+
+export default Loader;
