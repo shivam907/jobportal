5d6e282dc3a78177c3a036880a04b249d5b277c6
shivam907 <shivamkaushal907@gmail.com>
2024-02-24 14:36:15 +0530

    Added carrer pages

--- src/app/carrers/page.js
+++ src/app/carrers/page.js
@@ -1,28 +1,38 @@
+"use client";
 import React from "react";
 import Navbar from "@/components/Navbar/Navbar";
 import Footer from "@/components/Footer/Footer";
 import Hero from "@/components/Hero/Hero";
 import classes from "./page.module.css";
-import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import Link from "next/link";
+import data from "@/data/carrer.json";
 const page = () => {
+  const [company, setCompany] = React.useState();
+  React.useEffect(() => {
+    let arr = [];
+    data.forEach((i) => {
+      arr.push(
+        <Link href={i.link}>
+          <div className={classes.box}>
+            <div className={classes.img}>
+              <img src={i.img} alt="" />
+            </div>
+            <h1>{i.name}</h1>
+            {/* <div className={classes.icon}>
+            <ArrowForwardIcon className={classes.arrow}/>
+          </div> */}
+          </div>
+        </Link>
+      );
+    });
+    setCompany(arr);
+    console.log(data.length);
+  }, []);
   return (
     <>
       <Navbar />
       <Hero />
-      <div className={classes.body}>
-        <div className={classes.box}>
-          <div className={classes.img}>
-            <img
-              src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"
-              alt=""
-            />
-          </div>
-          <h1>Google</h1>
-          <div className={classes.icon}>
-            <ArrowForwardIcon className={classes.arrow}/>
-          </div>
-        </div>
-      </div>
+      <div className={classes.body}>{company}</div>
       <Footer />
     </>
   );
