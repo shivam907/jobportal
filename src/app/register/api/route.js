fff4c4a41d5c983d1fb81b07773baa2772801a22
shivam907 <shivamkaushal907@gmail.com>
2024-02-28 23:34:46 +0530

    added password hashing

--- src/app/register/api/route.js
+++ src/app/register/api/route.js
@@ -2,6 +2,7 @@
 import { cookies } from 'next/headers'
 import User from '@/lib/models/User.model';
 import { connectToDB } from "@/lib/mongoose";
+import bcryptjs from "bcryptjs";
 export async function POST(req) {
   const data = await req.json();
   connectToDB()
@@ -10,7 +11,13 @@ export async function POST(req) {
   if(ifuser.length>0){
     return Response.json({ exist: true });
   }
-  const user=new User(data);
+    const hashedPassword = await bcryptjs.hash(data.password, 8)
+  const user=new User({
+    email: data.email,
+    name: data.name,
+    college: data.college,
+    password: hashedPassword,
+  });
   await user.save()
   return Response.json({ exist: false });
 }
