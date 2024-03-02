8d7ebdb3b7e12eb78e3e07e1a03dd169306a331f
shivam907 <shivamkaushal907@gmail.com>
2024-03-02 21:49:35 +0530

    added loader

--- src/app/companies/page.js
+++ src/app/companies/page.js
@@ -1,14 +1,17 @@
 'use client';
 import JobBox from "@/components/Job/JobBox";
 import React from "react";
+import Loader from "@/components/Loader/Loader";
 export default function Home() {
     const [job, setJob] = React.useState();
+    const [loading, setLoading] = useState(true);
   React.useEffect(() => {
     const jobs = async () => {
       const a = await fetch("/admin/dashboard/jobs/api");
       const b = await a.json();
       let arr = [];
       b.jobs.forEach((i) => {
+        setLoading(true)
         arr.push(
           <JobBox
             key={Math.random(1) * 1000}
@@ -22,17 +25,18 @@ export default function Home() {
             location={i.location}
             role={i.type}
             description={i.description?.slice(0, 100)}
-            link={"https://h.co"}
+            link={i.link}
           />
         );
       });
       setJob(arr);
+      setLoading(false)
     };
     jobs();
   },[])
   return (
     <>
-     {job}
+     {loading?<Loader/>:job}
     </>
   );
 }
