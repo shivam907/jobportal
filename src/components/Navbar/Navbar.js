0579bb8a6fb6ed58b0cfa03a85786f9f58a9221a
shivam907 <shivamkaushal907@gmail.com>
2024-03-05 16:32:48 +0530

    added Logout button in nav

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,12 +2,20 @@ import React from "react";
 import classes from "./Navbar.module.css";
 import MenuIcon from "@mui/icons-material/Menu";
 import CloseIcon from '@mui/icons-material/Close';
-
+import { userLogin } from "@/lib/actions";
+import LogoutIcon from '@mui/icons-material/Logout';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 const Navbar = () => {
   const [scroll, changeScroll] = React.useState(false);
   const [open, changeOpen] = React.useState(false);
+  const [login, setLogin] = React.useState(false)
+  const [logout, setLogout] = React.useState(false)
+  const [name, setName] = React.useState()
   function change() {
     changeOpen(open ? false : true);
+  }
+  const logoutclick=()=>{
+    setLogout(logout?false:true)
   }
   const nav = React.useRef(null);
   React.useEffect(()=>{
@@ -17,9 +25,26 @@ const Navbar = () => {
       changeScroll(distanceY > shrinkOn ? true : false);
     };
     window.addEventListener("scroll", resizeHeaderOnScroll);
+    const fun=async()=>{
+      const res=await userLogin();
+      console.log(res);
+      if(res.loggedIn){
+        setLogin(true)
+      }
+      else{
+        setActive(true)
+      }
+    }
+    fun();
   },[])
   return (
-    <div ref={nav} className={`${classes.nav} ${scroll?classes.scroll:''} ${open?classes.zi:''}`}>
+    <>
+    <div
+      ref={nav}
+      className={`${classes.nav} ${scroll ? classes.scroll : ""} ${
+        open ? classes.zi : ""
+      }`}
+    >
       <nav>
         <h1>LOGO</h1>
         <div className={classes.navelements}>
@@ -28,29 +53,59 @@ const Navbar = () => {
             <div className={classes.navel}>Companies</div>
             <div className={classes.navel}>Jobs</div>
             <div className={classes.navel}>Carrers</div>
-            <div className={classes.navel}>Carrers</div>
           </div>
-          <div className={classes.nav2}>
-          {/* <Button2>Login</Button2> */}
-          <a>Register</a>
-          </div>
+          {login ? (
+            <div onClick={logoutclick} className={classes.logg}>
+              <p>Shivam</p>
+              <ArrowDropDownIcon className={classes.logIcon} />
+            </div>
+          ) : (
+            <div className={classes.nav2}>
+              <a>Register</a>
+            </div>
+          )}
         </div>
         <div className={classes.nav3}>
-          {open?<CloseIcon onClick={change} className={classes.menu}/>:<MenuIcon onClick={change} className={classes.menu}/>}
+          {open ? (
+            <CloseIcon onClick={change} className={classes.menu} />
+          ) : (
+            <MenuIcon onClick={change} className={classes.menu} />
+          )}
         </div>
+            {logout && <div className={classes.logout}>
+      <div className={classes.logbox}>
+        <div className={classes.lbox}>
+          <p>Logout</p>
+          <LogoutIcon/>
+        </div>
+      </div>
+    </div>}
       </nav>
-      {open && <div className={classes.mobile}>
-        <div className={classes.mob}>
-          <div className={classes.nav1}>
-            <div className={classes.navel}>About</div>
-            <div className={classes.navel}>Companies</div>
-            <div className={classes.navel}>Jobs</div>
-            <div className={classes.navel}>Carrers</div>
-            <div className={classes.navel}>Carrers</div>
+      {open && (
+        <div className={classes.mobile}>
+          <div className={classes.mob}>
+            <div className={classes.nav1}>
+              <div className={classes.navel}>About</div>
+              <div className={classes.navel}>Companies</div>
+              <div className={classes.navel}>Jobs</div>
+              <div className={classes.navel}>Carrers</div>
+            </div>
+            {login ? (
+              <div className={classes.logg}>
+                <p>Shivam</p>
+                <ArrowDropDownIcon />
+              </div>
+            ) : (
+              <div className={classes.nav2}>
+                <a>Register</a>
+              </div>
+            )}
           </div>
         </div>
-      </div>}
+      )}
+
     </div>
+    </>
   );
 };
 
