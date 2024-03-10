92d1902f8a353e1d0fb34b415a766466b303e8ca
shivam907 <shivamkaushal907@gmail.com>
2024-03-10 13:38:38 +0530

    added google analytics

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 import classes from "./page.module.css";
 import Loader from "@/components/Loader/Loader";
 import JobBox from "@/components/Job/JobBox";
@@ -11,9 +11,9 @@ export default function Home() {
   const [job, setJob] = React.useState();
   const [jobArray, setJobArray] = React.useState();
   const [loading, setLoading] = React.useState(true);
-  const [location, setLocation] = React.useState([])
-  const [experience, setExperience] = React.useState([])
-  const [filterData, setFilterData] = React.useState()
+  const [location, setLocation] = React.useState([]);
+  const [experience, setExperience] = React.useState([]);
+  const [filterData, setFilterData] = React.useState();
 
   React.useEffect(() => {
     const jobs = async () => {
@@ -24,8 +24,8 @@ export default function Home() {
       const b = await a.json();
       let arr = [];
       let temp = [];
-      let loc=[]
-      let exp=[]
+      let loc = [];
+      let exp = [];
       b.jobs.forEach((i) => {
         setLoading(true);
         temp.push({
@@ -42,8 +42,8 @@ export default function Home() {
           description: i.description?.slice(0, 100),
           link: i.link,
         });
-        loc.push(i.location)
-        exp.push(i.experience)
+        loc.push(i.location);
+        exp.push(i.experience);
         arr.push(
           <JobBox
             key={Math.random(1) * 1000}
@@ -61,17 +61,18 @@ export default function Home() {
           />
         );
       });
-      let nloc= [...new Set(loc)].sort()
-      let nexp = [...new Set(exp)].sort()
-      let nnloc=[], nnexp=[]
-      nloc.forEach(nl=>{
-        nnloc.push({"label":nl, "value":nl})
-      })
-      nexp.forEach(nl=>{
-        nnexp.push({"label":nl, "value":nl})
-      })
-      setLocation(nnloc)
-      setExperience(nnexp)
+      let nloc = [...new Set(loc)].sort();
+      let nexp = [...new Set(exp)].sort();
+      let nnloc = [],
+        nnexp = [];
+      nloc.forEach((nl) => {
+        nnloc.push({ label: nl, value: nl });
+      });
+      nexp.forEach((nl) => {
+        nnexp.push({ label: nl, value: nl });
+      });
+      setLocation(nnloc);
+      setExperience(nnexp);
       setJob(arr);
       setJobArray(temp);
       setLoading(false);
@@ -79,31 +80,37 @@ export default function Home() {
     };
     jobs();
   }, []);
-  console.log(location)
-  console.log(experience)
+  console.log(location);
+  console.log(experience);
   return (
     <>
       <Navbar />
       <main className={classes.main}>
-    <Hero jobArray={jobArray} setFilterData={setFilterData} location={location} experience={experience} />
+        <Hero
+          jobArray={jobArray}
+          setFilterData={setFilterData}
+          location={location}
+          experience={experience}
+        />
         <section className={classes.manual}>
           <div className={classes.newBoxes}>
             <div className={classes.box1}>
               <h1>View Latest Job Openings of 88+ Companies</h1>
               <Link href="/companies">
-              <div className={classes.btn3}>View Now</div>
+                <div className={classes.btn3}>View Now</div>
               </Link>
             </div>
             <div className={classes.box2}>
               <h1>View Carrer Pages of 50+ Companies</h1>
               <Link href="/carrers">
-              <div className={classes.btn3}>View Now</div>
+                <div className={classes.btn3}>View Now</div>
               </Link>
             </div>
           </div>
           <h1 className={classes.latest}>Latest Jobs</h1>
           <div className={classes.body}>
-          {loading ? <Loader /> :filterData?.length>0?filterData :job}</div>
+            {loading ? <Loader /> : filterData?.length > 0 ? filterData : job}
+          </div>
         </section>
       </main>
       <Footer />
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,140 +1,141 @@
 import React from "react";
 import classes from "./Navbar.module.css";
 import MenuIcon from "@mui/icons-material/Menu";
-import CloseIcon from '@mui/icons-material/Close';
+import CloseIcon from "@mui/icons-material/Close";
 import { userLogin } from "@/lib/actions";
 import Link from "next/link";
-import LogoutIcon from '@mui/icons-material/Logout';
-import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import LogoutIcon from "@mui/icons-material/Logout";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 const Navbar = () => {
   const [scroll, changeScroll] = React.useState(false);
   const [open, changeOpen] = React.useState(false);
-  const [login, setLogin] = React.useState(false)
-  const [logout, setLogout] = React.useState(false)
-  const [name, setName] = React.useState()
+  const [login, setLogin] = React.useState(false);
+  const [logout, setLogout] = React.useState(false);
+  const [name, setName] = React.useState();
   function change() {
     changeOpen(open ? false : true);
   }
-  const logoutclick=()=>{
-    setLogout(logout?false:true)
-  }
-  const logoutfun=async()=>{
-    setLogout(false)
-    setLogin(false)
-    await fetch("/api/logout")
-  }
+  const logoutclick = () => {
+    setLogout(logout ? false : true);
+  };
+  const logoutfun = async () => {
+    setLogout(false);
+    setLogin(false);
+    await fetch("/api/logout");
+  };
   const nav = React.useRef(null);
-  React.useEffect(()=>{
+  React.useEffect(() => {
     const resizeHeaderOnScroll = () => {
-      const distanceY = window.pageYOffset || document.documentElement.scrollTop,
+      const distanceY =
+          window.pageYOffset || document.documentElement.scrollTop,
         shrinkOn = 30;
       changeScroll(distanceY > shrinkOn ? true : false);
     };
     window.addEventListener("scroll", resizeHeaderOnScroll);
-    const fun=async()=>{
-      const res=await userLogin();
+    const fun = async () => {
+      const res = await userLogin();
       console.log(res);
-      if(res.loggedIn){
-        setName(res.loggedIn.name)
-        setLogin(true)
+      if (res.loggedIn) {
+        setName(res.loggedIn.name);
+        setLogin(true);
+      } else {
+        setLogin(false);
       }
-      else{
-        setLogin(false)
-      }
-    }
+    };
     fun();
-  },[])
+  }, []);
   return (
     <>
-    <div
-      ref={nav}
-      className={`${classes.nav} ${scroll ? classes.scroll : ""} ${
-        open ? classes.zi : ""
-      }`}
-    >
-      <nav>
-      <Link href="/">
-        <img className={classes.logo} src="/toid.png" alt="" />
-      </Link>
-        <div className={classes.navelements}>
-          <div className={classes.nav1}>
-            {/* <div className={classes.navel}>About</div> */}
-            <Link href="/companies">
-            <div className={classes.navel}>Companies</div>
-            </Link>
-            <Link href="https://cgcassignments.com">
-            <div className={classes.navel}>Notes</div>
-            </Link>
-            <Link href="/carrers">
-            <div className={classes.navel}>Carrers</div>
-            </Link>
-          </div>
-          {login ? (
-            <div onClick={logoutclick} className={classes.logg}>
-              <p>{name}</p>
-              <ArrowDropDownIcon className={classes.logIcon} />
-            </div>
-          ) : (
-            <Link href="/register">
-            <div className={classes.nav2}>
-              <a className={classes.reg}>Register</a>
-            </div>
-            </Link>
-          )}
-        </div>
-        <div className={classes.nav3}>
-          {open ? (
-            <CloseIcon onClick={change} className={classes.menu} />
-          ) : (
-            <MenuIcon onClick={change} className={classes.menu} />
-          )}
-        </div>
-            {logout && <div className={classes.logout}>
-      <div className={classes.logbox}>
-        <div onClick={logoutfun} className={classes.lbox}>
-          <p>Logout</p>
-          <LogoutIcon/>
-        </div>
-      </div>
-    </div>}
-      </nav>
-      {open && (
-        <div className={classes.mobile}>
-          <div className={classes.mob}>
+      <div
+        ref={nav}
+        className={`${classes.nav} ${scroll ? classes.scroll : ""} ${
+          open ? classes.zi : ""
+        }`}
+      >
+        <nav>
+          <Link href="/">
+            <img className={classes.logo} src="/toid.png" alt="" />
+          </Link>
+          <div className={classes.navelements}>
             <div className={classes.nav1}>
-              <div className={classes.navel}>About</div>            
+              {/* <div className={classes.navel}>About</div> */}
               <Link href="/companies">
-            <div className={classes.navel}>Companies</div>
-            </Link>
-            <Link href="https://cgcassignments.com">
-            <div className={classes.navel}>Notes</div>
-            </Link>
-            <Link href="/carrers">
-            <div className={classes.navel}>Carrers</div>
-            </Link>
+                <div className={classes.navel}>Companies</div>
+              </Link>
+              <Link href="https://cgcassignments.com">
+                <div className={classes.navel}>Notes</div>
+              </Link>
+              <Link href="/carrers">
+                <div className={classes.navel}>Carrers</div>
+              </Link>
             </div>
             {login ? (
               <div onClick={logoutclick} className={classes.logg}>
                 <p>{name}</p>
-                <ArrowDropDownIcon />
+                <ArrowDropDownIcon className={classes.logIcon} />
               </div>
             ) : (
               <Link href="/register">
-
-              <div className={classes.nav2}>
-                <a className={classes.reg}>Register</a>
-              </div>
+                <div className={classes.nav2}>
+                  <a className={classes.reg}>Register</a>
+                </div>
               </Link>
             )}
-          {logout && <div onClick={logoutfun} className={classes.lbox}>
-          <p>Logout</p>
-          <LogoutIcon/>
-        </div>}
           </div>
-        </div>
-      )}
-
-    </div>
+          <div className={classes.nav3}>
+            {open ? (
+              <CloseIcon onClick={change} className={classes.menu} />
+            ) : (
+              <MenuIcon onClick={change} className={classes.menu} />
+            )}
+          </div>
+          {logout && (
+            <div className={classes.logout}>
+              <div className={classes.logbox}>
+                <div onClick={logoutfun} className={classes.lbox}>
+                  <p>Logout</p>
+                  <LogoutIcon />
+                </div>
+              </div>
+            </div>
+          )}
+        </nav>
+        {open && (
+          <div className={classes.mobile}>
+            <div className={classes.mob}>
+              <div className={classes.nav1}>
+                <Link href="/companies">
+                  <div className={classes.navel}>Companies</div>
+                </Link>
+                <Link href="https://cgcassignments.com">
+                  <div className={classes.navel}>Notes</div>
+                </Link>
+                <Link href="/carrers">
+                  <div className={classes.navel}>Carrers</div>
+                </Link>
+              </div>
+              {login ? (
+                <div onClick={logoutclick} className={classes.logg}>
+                  <p>{name}</p>
+                  <ArrowDropDownIcon />
+                </div>
+              ) : (
+                <Link href="/register">
+                  <div className={classes.nav2}>
+                    <a className={classes.reg}>Register</a>
+                  </div>
+                </Link>
+              )}
+              {logout && (
+                <div onClick={logoutfun} className={classes.lbox}>
+                  <p>Logout</p>
+                  <LogoutIcon />
+                </div>
+              )}
+            </div>
+          </div>
+        )}
+      </div>
     </>
   );
 };
