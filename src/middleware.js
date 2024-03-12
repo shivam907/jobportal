25f0dcdafdc178be5b0cfbb1f596116b7c15702d
shivam907 <shivamkaushal907@gmail.com>
2024-03-12 21:29:34 +0530

    few change

--- src/middleware.js
+++ src/middleware.js
@@ -7,29 +7,5 @@ export function middleware(request) {
       // const data=adminLogin()
       // console.log(data)
   }
-  // const encryptedSessionData = cookies().get("session");
 
-  // const data = encryptedSessionData? jwt.verify(encryptedSessionData.value, 'secret'): false;
-  // // const data = encryptedSessionData ? JSON.parse(encryptedSessionData) : 0;
-  // if (request.nextUrl.pathname.startsWith("/admin/login")) {
-  //   console.log("log in")
-  //   console.log(data)
-  //   if(data.loggedIn){
-  //     return NextResponse.redirect(new URL("/admin/dashboard", request.url));
-  //   }
-  // }
-  // if (request.nextUrl.pathname.startsWith("/admin/dashboard")) {
-  //   console.log("dashboard")
-  //   if (data.loggedIn == false) {
-  //     console.log(data);
-  //     return NextResponse.redirect(new URL("/admin/login", request.url));
-  //   } else {
-  //     console.log(data);
-  //     NextResponse.next();
-  //   }
-  // }
 }
-
-// export const config = {
-//   matcher: "/admin/dashboard/:path*",
-// };
