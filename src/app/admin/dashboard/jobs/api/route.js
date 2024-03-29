37825f771d11ac1897b621cebc2dc97938d57a8f
shivam907 <shivamkaushal907@gmail.com>
2024-03-29 21:19:16 +0530

    fixed the caching isssue

--- src/app/admin/dashboard/jobs/api/route.js
+++ src/app/admin/dashboard/jobs/api/route.js
@@ -3,6 +3,7 @@ import { connectToDB } from "@/lib/mongoose";
 import Job from "@/lib/models/Job.model";
 export async function POST(req) {
   const data = await req.json();
+  console.log(data)
     connectToDB();
     const job=await Job.find();
     const njob=job.reverse()
